14235d88d7ece18feb1377653656a786785dea1b
DKMonster <davidkross@getusee.com>
2014-11-04 14:46:31 +0800

    My first black website.

---
+++ fire.app/config.rb
@@ -0,0 +1,25 @@
+# Require any additional compass plugins here.
+
+http_path = "/"
+css_dir = "../css"
+sass_dir = "sass"
+images_dir = "../img"
+javascripts_dir = "../js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# output_style意思則是指你的編譯出來的CSS碼要用哪種方式編譯出來。
+# 編譯共有下述四種方式：
+# expanded = 一般，每行CSS皆會斷行
+# nested = 有縮進，較好閱讀
+# compact = 簡潔格式，匯出來的ＣＳＳ檔案大小比上面兩個還小。
+#  compressed = 壓縮過的CSS，所有設定都以一行來進行排列。
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+preferred_syntax = :sass
